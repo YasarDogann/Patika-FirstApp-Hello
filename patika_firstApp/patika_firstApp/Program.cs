a99c18f9fe83db11effef55f94514dadaf0b6483
Yaşar Doğan <143627950+YasarDogann@users.noreply.github.com>
2024-10-01 08:32:17 +0300

    Add files via upload

---
+++ patika_firstApp/patika_firstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yaşar Doğan \nPatika'nın verdiği eğitim ile başarılı olup, " +
+                "sağlamış olduğu network sayesinde profesyonel yazılım kariyerime başlamak istiyorum");
+
+            Console.ReadKey();
+        }
+    }
+}
